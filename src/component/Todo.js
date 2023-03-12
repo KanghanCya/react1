68644b23154992b727b9debe5463f51ebc10078e
Kanghan <kanghan123456789@gmail.com>
2023-03-12 21:17:41 +0700

    first commit

---
+++ src/component/Todo.js
@@ -0,0 +1,15 @@
+
+
+function Todo() {
+  return (
+    <div>
+      <h1>I am Atomic</h1>
+        <div className="card">
+         <h2>TiTle</h2>
+         <button className="btn">click me</button>
+        </div>
+    </div>
+  );
+}
+
+export default Todo;
